035f06e44a3bceab6652e24b6756bcfb7f92ae1a
Valentin Panasyuk <v.panasuk@appkode.ru>
2023-10-26 22:46:52 +0300

    Add props type to character list

--- src/shared/components/characters-list/characters-list.tsx
+++ src/shared/components/characters-list/characters-list.tsx
@@ -1,17 +1,19 @@
 import classnames from 'classnames'
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import { CharacterCard } from '../character-card'
 import styles from './characters-list.module.scss'
+import { Character } from '../../interfaces';
 
-export const CharactersList = ({ characters, className }: any) => {
+type Props = {
+  characters: Character[]
+  className: string
+}
 
-  useEffect(() => {
-    console.log(className)
-  })
+export const CharactersList = ({ characters, className }: Props) => {
   return (
     <ul className={classnames(styles.wrapper, className)}>
-      {characters.map(({ id, name, image, status, species, gender }: any) => (
+      {characters.map(({ id, name, image, status, species, gender }) => (
         <li key={id}>
           <CharacterCard
             name={name}
